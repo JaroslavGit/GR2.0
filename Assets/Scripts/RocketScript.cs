f17a93e9c54b8128ba0a270c2c4c56ba1f6c4edc
unknown <theo.krok@seznam.cz>
2023-01-08 14:47:55 +0100

    Fuel Counter

--- Assets/Scripts/RocketScript.cs
+++ Assets/Scripts/RocketScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RocketScript : MonoBehaviour
 {
@@ -16,19 +17,32 @@ public class RocketScript : MonoBehaviour
     [SerializeField]
     public int Health = 100;
 
+     [SerializeField]
+     public float fuelAmount;
+     public Text fuelMeter;
+    private bool canFly;
+
+
     // Start is called before the first frame update
     void Start()
     {
         shotSound = GetComponent<AudioSource>();
         barelSound = GetComponent<AudioSource>();
+        fuelAmount = 100;
+       canFly = true;
+       
     }
 
     // Update is called once per frame
     void Update()
-    {
+    { 
+     if(fuelAmount > 0)
+     {
         MoveH();
         Fire();
-       
+        canFly = false;
+     } 
+     fuelMeter.text = "Fuel: " + fuelAmount.ToString() + "%";    
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -45,21 +59,45 @@ public class RocketScript : MonoBehaviour
     //poohyb horizontalne
     void MoveH()
     {
+     
+
         inputMove = Input.GetAxisRaw("Horizontal");
         transform.position += new Vector3(inputMove, 0f, 0f) * speed * Time.deltaTime;
-
+         StartCoroutine(BurnFuel());
+        
     }
 
     //strelba
     void Fire()
     {
+        if(fuelAmount >= 10)
+        {
         if (Input.GetKeyDown(KeyCode.Space))
           {
             Debug.Log("Strilim");
-            //vytvoøím strelu
+            //vytvoï¿½ï¿½m strelu
             Instantiate(bullet, transform.position, Quaternion.identity);
             shotSound.Play();
+            fuelAmount -= 10 ;
           }
-
+        }
     }
+
+    private IEnumerator BurnFuel()
+    {
+        if( canFly)
+        {
+               for(float i = fuelAmount; i >= 1; i--)
+        {
+            if(fuelAmount > 0)
+            {
+            fuelAmount -= 1f;
+            yield return new WaitForSeconds(1);
+            }else{
+                fuelAmount -= 0f;
+            }
+        }
+        }
+    }
+
 }
